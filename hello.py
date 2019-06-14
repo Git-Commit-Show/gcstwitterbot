562c1e16595c751df74d1f71dccada68250dea48
Parth Krishna <thenerdsuperuser@gmail.com>
2019-06-14 22:06:04 +0530

    this bot tweets :D

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import tweepy
 from data import *
+import datetime
 
+
+today = datetime.datetime.now().strftime("%A, %d %B %Y, %I:%M %p")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -10,3 +13,7 @@ api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+
+# Post a tweet from Python
+api.update_status("Okay I guess it is working. Let me make it unique. @pradeep_io, the date time is : " +today + "\n\n #gitshowcommit")
