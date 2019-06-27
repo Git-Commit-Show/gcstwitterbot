128dc8f0bcf1fcda5b651b74380d7e988a47ca3a
Parth Krishna <thenerdsuperuser@gmail.com>
2019-06-27 22:58:29 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
-consumer_key, consumer_secret = "oWjmE4TTVs6o25AVPgF0Kxs0w", "CvvHWBuyj01OIVlu0sSjZca4OJVKwibjXkthB0qWLm5caK2WSc"
-
-access_token, access_token_secret = "1139468082957545473-hOD3ODDUzTtuUdy0YItPDiRBQgLZ28", "IaSxPzEujBOJdZd3oPeqZ9Eoy0nItXfgOvQez3DpB5lzq"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
