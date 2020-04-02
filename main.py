f22b49e92cd486f213189c2b706d062895f6450b
Parth Krishna <thenerdsuperuser@gmail.com>
2020-04-02 13:07:34 +0530

    optimised and refactored the code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from time import gmtime, strftime
 
 bot_username = 'gitcommitshow'
 logfile_name = bot_username + ".log"
+errorfile_name = "errors.log"
 
 # Auth
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -16,30 +17,6 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-"""
-try:
-    api.create_friendship("vimoveremacs")
-
-except tweepy.error.TweepError as e:
-    log(e.message)
-
-else:
-    log("Tweeted: " + text)
-"""
-
-class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-
-    def on_status(self, tweet):
-        uname = tweet.user.name
-        text = tweet.text
-        log(uname + ' said ' + text + "\n")
-        print(uname + ' said ' + text + "\n")
-    def on_error(self, status):
-        print("Error detected")
 
 
 def log(message):
@@ -50,6 +27,46 @@ def log(message):
         f.write("\n" + t + " " + message)
 
 
+def ErrorLog(message):
+    """Log message to logfile."""
+    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(path, errorlogfile_name), 'a+') as f:
+        t = strftime("%d %b %Y %H:%M:%S", gmtime())
+        f.write("\n" + t + " " + message)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        uname = tweet.user.name
+        text = tweet.text
+        try:
+            tweet.favorite()
+            tweet.user.follow()
+        except tweepy.error.TweepError as e:
+            ErrorLog(e.message)
+        log(uname + ' said ' + text + "\n")
+        print(uname + ' said ' + text + "\n")
+
+    '''
+    def mentions(self, tweet):
+        myMentions = api.mentions_timeline()
+        for tweet in myMentions:
+            try:
+                tweet.favorite()
+                tweet.user.follow()
+
+            except tweepy.error.TweepError as e:
+            ErrorLog(e.message)
+    '''
+
+    def on_error(self, status):
+        print("Error detected")
+
+
 def timeline():
     timeline = api.home_timeline()
     for tweet in timeline:
@@ -57,6 +74,9 @@ def timeline():
         text = tweet.text
 
         log(uname + ' said ' + text + "\n")
+
+
+
 
 if __name__ == "__main__":
     #timeline()
