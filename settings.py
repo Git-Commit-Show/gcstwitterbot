be55482bd850998795e8687bb20e7b30de0572ac
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-20 10:07:41 +0530

    Added global settings file, for easy modifications

---
+++ settings.py
@@ -0,0 +1,8 @@
+FAVORITE = True
+RETWEET = True
+FOLLOW = True
+MAIL = True
+DELAY = 60 # 300 suggested (in seconds which is 5 mins)
+HASHTAG_LIST = ["#gitcommitshow", "#GitCommitShow", "gitcommitshow", "GitCommitShow"]
+
+
