f3d6a9f4334bfb15457d170c0876ec2b3fcc4d05
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-22 12:59:12 +0530

    Added default autoreply

--- autoreply.py
+++ autoreply.py
@@ -6,8 +6,6 @@ from time import gmtime, strftime, sleep
 from datetime import datetime
 import json
 from send_mail import *
-from settings import *
-from ignore import *
 
 
 now = datetime.now()
@@ -17,7 +15,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def check_mentions(api, keywords, since_id):
-    keyword = ""
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
@@ -28,17 +25,9 @@ def check_mentions(api, keywords, since_id):
         tweet_id = tweet.id
         if tweet.in_reply_to_status_id is not None:
             continue
-        if any(keyword in tweet.text.lower() for keyword in keys):
-            if tweet_id in IGNORE.values():
-                logger.warning("Skipping " + tweet.user.name)
-            else:
-                IGNORE.update(uname = tweet_id)
-                logger.info("Replying to " + tweet.user.name)
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info("Answering to " + tweet.user.name)
 
-                text = "@" + tweet.user.screen_name
-                tweet_text = text + KEYWORDS[keyword]
-                print(tweet_text)
-            '''
             if not tweet.user.following:
                 try:
                     tweet.user.follow()
@@ -50,6 +39,9 @@ def check_mentions(api, keywords, since_id):
                     body = "{0} \n\nOccured at {1}".format(mess, timestr)
                     mail(sub, body)
                     ErrorLog("[AUTO ERROR]" + str(e))
+
+            text = "@" + tweet.user.screen_name
+            tweet_text = text + " please reach us via DM"
             try:
                 api.update_status(
                     status=tweet_text,
@@ -63,19 +55,14 @@ def check_mentions(api, keywords, since_id):
                 body = "{0} \n\nOccured at {1}".format(mess, timestr)
                 mail(sub, body)
                 ErrorLog("[AUTO ERROR]" + str(e))
-            '''
+
     return new_since_id
 
-
-# We have to explicitly convert KEYWORDS.keys() into list. Default - dict_keys()
-keys = list(KEYWORDS.keys())
-
 def main():
-    global keys
     api = create_api()
     since_id = 1
     while True:
-        since_id = check_mentions(api, list(KEYWORDS.keys()), since_id)
+        since_id = check_mentions(api, ["help", "support"], since_id)
         logger.info("Waiting...")
         sleep(60)
 
